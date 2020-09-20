4ebc2573b97d3cde59cd8e00da24a408c9591763
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-09-20 22:20:57 +0200

    fix reactor ping test

--- reactor/reactor-st/src/test/java/airhacks/service/ping/boundary/PingResourceIT.java
+++ reactor/reactor-st/src/test/java/airhacks/service/ping/boundary/PingResourceIT.java
@@ -1,12 +1,14 @@
 package airhacks.service.ping.boundary;
-import java.net.URI;
-import javax.ws.rs.core.Response;
-import org.eclipse.microprofile.rest.client.RestClientBuilder;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  *
@@ -14,15 +16,15 @@ import org.junit.jupiter.api.Test;
  */
 public class PingResourceIT {
 
-    private PostsResourceClient client;
+    private ReactorResourceClient client;
 
     @BeforeEach
     public void init() {
-        URI uri = URI.create("http://localhost:8081/reactor/resources/");
+        URI uri = URI.create("http://localhost:8080/reactor/resources/");
         this.client = RestClientBuilder.
                 newBuilder().
                 baseUri(uri).
-                build(PostsResourceClient.class);
+                build(ReactorResourceClient.class);
 
     }
 
