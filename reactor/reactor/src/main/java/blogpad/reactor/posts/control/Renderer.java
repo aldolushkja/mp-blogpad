c14c9c6bd9a667e5ab1da2865d9e686d2f63665b
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-10-23 19:40:08 +0200

    counting rendering errors

--- reactor/reactor/src/main/java/blogpad/reactor/posts/control/Renderer.java
+++ reactor/reactor/src/main/java/blogpad/reactor/posts/control/Renderer.java
@@ -1,9 +1,13 @@
 package blogpad.reactor.posts.control;
 
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.annotation.RegistryType;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -12,16 +16,29 @@ public class Renderer {
 
     Source handlebars;
 
+    @Inject
+    @RegistryType(type= MetricRegistry.Type.APPLICATION)
+    MetricRegistry registry;
+
     @PostConstruct
     public void init() {
         try {
             this.handlebars = Source.newBuilder("js", this.loadHandlebars(), "Handlebars").build();
         } catch (IOException e) {
+            this.registry.counter("rendering_errors").inc();
             throw new IllegalStateException("cannot load handlebars", e);
         }
     }
 
     public String render(String templateContent, String postContent) {
+        try {
+            return this.unsafeRender(templateContent, postContent);
+        } catch (PolyglotException e) {
+            throw new IllegalStateException("Rendering error, reason: " + e.getMessage(), e);
+        }
+    }
+
+    String unsafeRender(String templateContent, String postContent) {
         try (Context context = Context.create("js")) {
             var bindings = context.getBindings("js");
             context.eval(this.handlebars);
