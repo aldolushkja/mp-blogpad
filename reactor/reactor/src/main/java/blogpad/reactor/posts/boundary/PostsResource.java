0f842d46ff1dc87ae158f9fdb26facc517179ad4
alushkja <aldo.lushkja@consulenti.fastweb.it>
2020-10-23 20:10:34 +0200

    overload protection with bulkheads

--- reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
+++ reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
@@ -1,5 +1,7 @@
 package blogpad.reactor.posts.boundary;
 
+import org.eclipse.microprofile.faulttolerance.Bulkhead;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.metrics.annotation.ConcurrentGauge;
 import org.eclipse.microprofile.opentracing.Traced;
 
@@ -9,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("posts")
 public class PostsResource {
@@ -18,11 +21,21 @@ public class PostsResource {
 
     @GET
     @Traced
+    @Fallback(fallbackMethod = "overloadedServer")
+    @Bulkhead(2)
     @ConcurrentGauge
     @Path("{title}")
     @Produces(MediaType.TEXT_HTML)
-    public String findPost(@PathParam("title") String title) {
-        return this.reactor.render(title);
+    public Response findPost(@PathParam("title") String title) {
+        var content = this.reactor.render(title);
+        return Response.ok(content).build();
+    }
+
+    public Response overloadedServer(String title) {
+        return Response.status(503).
+                header("reason", "overloaded server").
+                header("affected_post", title).
+                build();
     }
 
 }
